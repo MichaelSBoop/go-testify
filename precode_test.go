4232f04e358331dd33e14f46681057c6feea457a
Michael Stepanischevskiy <mihail.mike@mail.ru>
2024-04-23 09:55:26 +0300

    initial task state

--- precode_test.go
+++ precode_test.go
@@ -12,17 +12,12 @@ import (
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?city=moscow&count=10", nil) // здесь нужно создать запрос к сервису
+	req := ... // здесь нужно создать запрос к сервису
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	// Status check: if status != 200, no need to continue
-	status := responseRecorder.Code
-	require.Equal(t, http.StatusOK, status)
-
-	// Check behaviour if requested count exceeds total count
-	recievedCount := strings.Split(responseRecorder.Body.String(), ",")
-	assert.Equal(t, len(recievedCount), totalCount)
+	// здесь нужно добавить необходимые проверки
 }
+
